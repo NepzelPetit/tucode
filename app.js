b58a1fd8076b93f4f924f86b858e73574d09e0b7
DragonWebTools <dragon.uikit@gmail.com>
2021-05-09 14:10:36 -0400

    creacion

---
+++ app.js
@@ -0,0 +1,35 @@
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+// class server {
+// 	constructor () {
+// 		this.express = require('express')();
+// 		this.port = 8000;
+// 	}
+// 	get(route,exec) {
+// 		this.express.get(route, (req, res) => {
+// 			let data = exec();
+// 			res.send(data);
+// 		})
+// 	}
+// 	run(log = `Example app listening at http://localhost:${this.port}`) {
+// 		let port = this.port;
+// 		this.express.listen(port,() => {
+// 			console.log(log)
+// 		})
+// 	}
+// }
+
+// const App = new server;
+
+// App.get('/hola', () => {
+// 	return 'hola a todos';
+// })
+
+// App.run();
+
